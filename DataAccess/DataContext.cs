7dca0048eb4988b315ed70bdc4daef5d7e6a46e8
Maks Michalski <mamich1998@outlook.com>
2022-02-13 11:17:19 +0100

    Add entity to project

--- DataAccess/DataContext.cs
+++ DataAccess/DataContext.cs
@@ -1,6 +1,30 @@
-﻿namespace DataAccess
+﻿using Common.Settings;
+using DataAccess.Extensions;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionstring = config.GetConnectionString(nameof(ConnectionStrings.DbConnectionString));
+
+            builder.UseSqlServer(connectionstring);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.SetUser();
+        }
     }
 }
---
+++ DataAccess/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Linq.Expressions;
+
+namespace DataAccess.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void SetRefreshToken(this ModelBuilder builder)
+        {
+            builder.SetProperty<RefreshToken>(x => x.CreationDate, requiered: true);
+            builder.SetProperty<RefreshToken>(x => x.ExpireDate, requiered: true);
+            builder.SetProperty<RefreshToken>(x => x.Token, requiered: true);
+        }
+
+        public static void SetUser(this ModelBuilder builder)
+        {
+            builder.SetProperty<User>(x => x.Email, 50, true);
+            builder.SetProperty<User>(x => x.FirstName, 50, true);
+            builder.SetProperty<User>(x => x.LastName, 50, true);
+            builder.SetProperty<User>(x => x.HashPassword, requiered: true);
+        }
+
+        private static PropertyBuilder SetProperty<T>(this ModelBuilder modelBuilder,
+            Expression<Func<T, object>> property,
+            int? stringLenght = null,
+            bool requiered = false) where T : class
+        {
+            var builder = modelBuilder.Entity<T>()
+                .Property(property);
+
+            if (stringLenght.HasValue)
+            {
+                builder.HasMaxLength(stringLenght.Value);
+            }
+
+            if (requiered)
+            {
+                builder.IsRequired();
+            }
+
+            return builder;
+        }
+    }
+}
