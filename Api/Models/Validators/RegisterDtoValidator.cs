22575b7672efdb45dbf656c555ce47555c783a8c
Maks Michalski <maks.michalski@pspg.pl>
2022-04-01 13:23:17 +0200

    Dodanie walidacji do rejestracji

--- Api/Models/Validators/RegisterDtoValidator.cs
+++ Api/Models/Validators/RegisterDtoValidator.cs
@@ -6,6 +6,18 @@ namespace Api.Models.Validators
     {
         public RegisterDtoValidator()
         {
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .Length(10, 50);
+            RuleFor(x => x.FirstName)
+                .Length(1, 50)
+                .NotEmpty();
+            RuleFor(x => x.LastName)
+                .Length(1, 50)
+                .NotEmpty();
+            RuleFor(x => x.Password)
+                .Length(5, 25)
+                .NotEmpty();
         }
     }
 }
