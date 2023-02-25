30650fd8a27d9278a18183292cdd3be03adebb93
Maks Michalski <mamich1998@outlook.com>
2023-02-25 14:56:18 +0100

    Poprawki na autoryzacji

--- ApplicationCore/Interfaces/ICookieService.cs
+++ ApplicationCore/Interfaces/ICookieService.cs
@@ -5,4 +5,6 @@ public interface ICookieService
     void AddCookie(string name, string value, int expire, bool httpOnly);
 
     string GetCookie(string name);
+
+    public void RemoveCookie(string name);
 }
