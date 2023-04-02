2b94116eaffbf487687de1f993c857899923548d
Maks Michalski <mamich1998@outlook.com>
2023-04-02 12:11:38 +0200

    Utworzenie endpointa dla notyfikacji

---
+++ Api/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Bases;
+using ApplicationCore.Cqrs.Notification.Delete;
+using ApplicationCore.Cqrs.Notification.Get;
+using ApplicationCore.Cqrs.Notification.Update;
+using ApplicationCore.Dtos.Notification;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class NotificationController : BaseApiController
+{
+    public NotificationController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<bool>> DeleteAsync([FromRoute] Guid id)
+        => await ApiResponseAsync<bool, DeleteNotificationCommand>(new(id));
+
+    [HttpGet("{recipientId}")]
+    public async Task<ActionResult<IEnumerable<NotificationDto>>> GetsByRecipientIdAsync([FromRoute] Guid recipientId)
+        => await ApiResponseAsync<IEnumerable<NotificationDto>, GetNotificationsByRecipientIdQuery>(new(recipientId));
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<NotificationDto>> UpdateAsync([FromRoute] Guid id, [FromBody] NotificationInputDto dto)
+        => await ApiResponseAsync<NotificationDto, UpdateNotificationCommand>(new(id, dto));
+}
